4df9046b823ce055c4061b09a2cbad7235827908
Maicol <maicolcrodrigoa@gmail.com>
2020-08-14 14:04:45 -0500

    morgan configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,8 +17,50 @@ module.exports = {
     jest: true,
   },
   rules: {
+    'no-trailing-spaces': 'error',
+    'eol-last': ['error', 'always'],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    semi: ['error', 'always'],
+    'linebreak-style': 0,
+    'no-empty': ['error', { allowEmptyCatch: true }],
+    'array-element-newline': ['error', 'consistent'],
+    'array-bracket-spacing': ['error', 'never'],
+    'array-bracket-newline': ['error', { multiline: true }],
+    'object-curly-spacing': ['error', 'always'],
+    'object-property-newline': 'error',
+    'object-curly-newline': [
+      'error',
+      {
+        multiline: true,
+        minProperties: 3,
+      },
+    ],
+    'key-spacing': ['error', { afterColon: true }],
+    'comma-spacing': [
+      'error',
+      {
+        before: false,
+        after: true,
+      },
+    ],
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'never',
+        objects: 'never',
+        imports: 'never',
+        exports: 'never',
+        functions: 'never',
+      },
+    ],
+    /**real */
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+
+    //fake
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
 };
